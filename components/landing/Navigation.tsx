6174d18a65d5bb37fdcdae5f0f9e27ff933d6584
Suryansh777777 <suryanshchourasia12@gmail.com>
2025-10-05 00:57:42 +0530

    refactor: Remove deprecated components and update feature integration
    - Deleted unused components: EffortlessIntegration, NumbersThatSpeak, SmartSimpleBrilliant, YourWorkInSync.
    - Updated Features component to integrate BentoIcons in place of removed components.
    - Enhanced layout and styling for improved responsiveness and visual consistency.

--- components/landing/Navigation.tsx
+++ components/landing/Navigation.tsx
@@ -17,7 +17,7 @@ export function Navigation() {
               {name}
             </div>
           </Link>
-          <div className="pl-3 sm:pl-4 md:pl-5 lg:pl-5 flex justify-start items-start hidden sm:flex flex-row gap-2 sm:gap-3 md:gap-4 lg:gap-4">
+          <div className="pl-3 sm:pl-4 md:pl-5 lg:pl-5  justify-start items-start hidden sm:flex flex-row gap-2 sm:gap-3 md:gap-4 lg:gap-4">
             {nav.links.map((link) => (
               <div key={link.name} className="flex justify-start items-center">
                 <div className="flex flex-col justify-center text-[rgba(49,45,43,0.80)] text-xs md:text-[13px] font-medium leading-[14px] font-sans">
